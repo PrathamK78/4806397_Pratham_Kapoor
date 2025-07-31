34e0364802545cf8e2dfebddaee23a237053ad67
Pratham Kapoor <prathamkapoor1906@gmail.com>
2025-07-31 12:40:36 +0530

    Create a practice C file

--- SDLC/C_programming/test.c
+++ SDLC/C_programming/test.c
@@ -3,6 +3,6 @@
 int main() {
     printf("Hello, World! my name is the great C programmer.\n");
     printf("This program is written in C11 standard.\n");
-    printf("Let's write some C code!\n");
+    printf("Let us write some C code!\n");
     return 0;
 }
