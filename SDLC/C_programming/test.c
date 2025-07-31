b613b7613cf4be0c1ba88df1582b8c95c3124aea
Pratham Kapoor <prathamkapoor1906@gmail.com>
2025-07-31 12:37:58 +0530

    Add new commands

---
+++ SDLC/C_programming/test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello, World! my name is the great C programmer.\n");
+    printf("This program is written in C11 standard.\n");
+    printf("Let's write some C code!\n");
+    return 0;
+}
