aa57382939a83f52d15f05956f1f227158316c55
Pratham Kapoor <prathamkapoor1906@gmail.com>
2025-08-16 21:44:39 +0530

    Update week 2 solutions at day 7

---
+++ HackerRank_Solutions/Week_2/CounterGame.java
@@ -0,0 +1,20 @@
+package HackerRank_Solutions.Week_2;
+
+import java.io.*;
+import java.util.*;
+
+public class CounterGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        
+        for (int t = 0; t < T; t++) {
+            long n = scanner.nextLong();
+          //  System.out.println(counterGame(n));
+        }
+        
+        scanner.close();
+    }
+
+    
+}
