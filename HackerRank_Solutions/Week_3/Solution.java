8a0d9bb0e314cdcfa7f4bfeffc719825abcba42d
Pratham Kapoor <prathamkapoor1906@gmail.com>
2025-08-22 22:16:17 +0530

    Update week 3 solutions

---
+++ HackerRank_Solutions/Week_3/Solution.java
@@ -0,0 +1,107 @@
+package HackerRank_Solutions.Week_3;
+
+import java.io.*;
+import java.util.stream.*;
+
+
+class SinglyLinkedListNode {
+    public int data;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    public SinglyLinkedListNode head;
+    public SinglyLinkedListNode tail;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertNode(int nodeData) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+}
+
+class SinglyLinkedListPrintHelper {
+    public static void printList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
+
+class Result {
+
+    public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position) {
+    // Write your code here
+         SinglyLinkedListNode curr = llist;
+        int count = 1;
+        while(curr!=null){
+            if(count == position){
+                SinglyLinkedListNode next_node = new SinglyLinkedListNode(data);
+                SinglyLinkedListNode curr_node = curr.next;
+                
+                curr.next = next_node;
+                next_node.next = curr_node;
+            }
+        curr = curr.next;
+        count++;
+        }
+        return llist;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, llistCount).forEach(i -> {
+            try {
+                int llistItem = Integer.parseInt(bufferedReader.readLine().trim());
+
+                llist.insertNode(llistItem);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int data = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int position = Integer.parseInt(bufferedReader.readLine().trim());
+
+        SinglyLinkedListNode llist_head = Result.insertNodeAtPosition(llist.head, data, position);
+
+        SinglyLinkedListPrintHelper.printList(llist_head, " ", bufferedWriter);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
